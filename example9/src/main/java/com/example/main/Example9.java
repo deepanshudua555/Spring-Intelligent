e2a1ba9bb9b85b4f9160992c008fc7d7453927d2
deepanshudua555 <deepanshudua555@gmail.com>
2025-01-28 01:24:26 +0530

    wiring in spring

---
+++ example9/src/main/java/com/example/main/Example9.java
@@ -0,0 +1,23 @@
+package com.example.main;
+
+import com.example.bean.Person;
+import com.example.bean.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Example9 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Person name from Spring Context is: " + person.getName());
+        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
+        System.out.println("Vehicle that Person own is: " + person.getVehicle());
+
+    }
+}
