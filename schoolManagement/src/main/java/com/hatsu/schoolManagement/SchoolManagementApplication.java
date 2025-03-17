4e57f878d799cb33d28a71c1aedebad7657b50ce
deepanshudua555 <deepanshudua555@gmail.com>
2025-03-18 00:29:42 +0530

    started school management app

--- schoolManagement/src/main/java/com/hatsu/schoolManagement/SchoolManagementApplication.java
+++ schoolManagement/src/main/java/com/hatsu/schoolManagement/SchoolManagementApplication.java
@@ -3,11 +3,14 @@ package com.hatsu.schoolManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SchoolManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolManagementApplication.class, args);
+
 	}
 
 }
+
---
+++ schoolManagement/src/main/java/com/hatsu/schoolManagement/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.hatsu.schoolManagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping({"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Deepanshu Dua");
+        return "home.html";
+    }
+
+}
